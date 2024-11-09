9c9fb285d5f75017641473cc5c42680c78f34fbb
강민재 <minijae011030@naver.com>
2024-11-09 19:41:23 +0900

    create project page

--- src/Project/page/CreateProjectPage.tsx
+++ src/Project/page/CreateProjectPage.tsx
@@ -1,9 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../style/project.module.css";
+import Select from "react-select";
+import { TextInput, TagsInput, Button } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 
 const CreateProjectPage: React.FC = () => {
+  const navigation = useNavigate();
+
+  const [projectTitle, setProjectTitle] = useState<string>("");
+  const [projectType, setProjectType] = useState<{
+    value: string;
+    label: string;
+  } | null>(null);
+  const [inputValue, setInputValue] = useState("");
+  const [inviteUsers, setInviteUsers] = useState<string[]>([]);
+
+  const options = [
+    { value: "개인", label: "개인 프로젝트" },
+    { value: "회사", label: "회사 프로젝트" },
+    { value: "동아리", label: "동아리 프로젝트" },
+    { value: "학교", label: "학교 프로젝트" },
+  ];
+
+  function handleProjectTypeChange(
+    selectedOption: { value: string; label: string } | null
+  ) {
+    setProjectType(selectedOption);
+  }
+
+  function handleInputChange(newValue: string) {
+    setInputValue(newValue);
+  }
+
+  function CancelCreateProject() {
+    const result = window.confirm(
+      "현재까지의 내용은 저장되지 않습니다.\n그래도 취소하시겠습니끼?"
+    );
+    if (result) navigation("/main");
+  }
+
   return (
-    <div>
-      create_button<p>creaets</p>
+    <div className={styles.outer_container}>
+      <div className={styles.main_container}>
+        <p className={styles.main_title}>프로젝트 생성</p>
+        <div className={styles.inner_container}>
+          <div className={styles.title}>
+            <p>프로젝트 명</p>
+            <TextInput
+              value={projectTitle}
+              onChange={(event) => setProjectTitle(event.currentTarget.value)}
+              placeholder="프로젝트 제목을 입력해주세요"
+              style={{ width: "100%", maxWidth: "530px" }}
+            />
+          </div>
+
+          <TagsInput
+            value={inviteUsers}
+            onChange={setInviteUsers}
+            placeholder="초대할 유저 아이디를 입력하고 Enter 키를 눌러 추가하세요"
+          />
+
+          <Select
+            options={options}
+            value={projectType}
+            onChange={handleProjectTypeChange}
+            placeholder="프로젝트 유형을 선택하세요"
+            inputValue={inputValue}
+            onInputChange={handleInputChange}
+          />
+        </div>
+        <div className={styles.button}>
+          <Button
+            onClick={() => console.log("hello")}
+            variant="filled"
+            color="rgb(173, 216, 230)"
+          >
+            프로젝트 생성하기
+          </Button>
+          <Button onClick={CancelCreateProject} variant="default">
+            취소
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
