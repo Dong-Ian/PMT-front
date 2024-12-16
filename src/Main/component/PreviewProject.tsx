fad3664bb4dc8d72650577d775e17da1fd1431d0
강민재 <minijae011030@naver.com>
2024-12-16 21:42:26 +0900

    Feat: Preview 렌더링
    프로젝트 preview map으로 렌더링 wip

--- src/Main/component/PreviewProject.tsx
+++ src/Main/component/PreviewProject.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "../style/main.module.css";
+import { PreviewProjectProps } from "../type/Main.type";
 
-const PreviewProject: React.FC = () => {
+const PreviewProject: React.FC<PreviewProjectProps> = (props) => {
+  console.log(props);
   return (
     <div className={styles.create_button}>
       <p>preview</p>
--- src/Main/page/MainPage.tsx
+++ src/Main/page/MainPage.tsx
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../style/main.module.css";
 import CreateProjectButton from "../component/CreateProjectButton";
 import PreviewProject from "../component/PreviewProject";
 import { useRecoilValue } from "recoil";
 import { tokenState } from "../../Utils/Atom/Atom";
 import GetProjectListFunction from "../function/GetProjectListFunction";
+import { PreviewProjectProps } from "../type/Main.type";
 
 const MainPage: React.FC = () => {
   const token = useRecoilValue(tokenState);
+  const [previewProjectList, setPreviewProjectList] = useState<
+    PreviewProjectProps[] | []
+  >([]);
 
   async function GetProjectList() {
     const result = await GetProjectListFunction({ token });
+    if (result.code === "0000") {
+      setPreviewProjectList(result.result);
+      return;
+    }
+
+    alert("프로젝트 정보를 불러오지 못하였습니다.");
+    return;
   }
 
   useEffect(() => {
@@ -23,10 +34,9 @@ const MainPage: React.FC = () => {
         <p className={styles.title}>Project</p>
         <div className={styles.inner_container}>
           <CreateProjectButton />
-          <PreviewProject />
-          <PreviewProject />
-          <PreviewProject />
-          <PreviewProject />
+          {previewProjectList.map((project, index) => {
+            return <PreviewProject key={index} {...project} />;
+          })}
         </div>
       </div>
     </div>
