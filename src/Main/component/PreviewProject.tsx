97194562f7732f23699fc3539ca05c63d1936b14
강민재 <minijae011030@naver.com>
2024-12-17 16:08:14 +0900

    Feat: Preview Navigation
    preview box 클릭 시projectSeq를 통해 해당 프로젝트로 이동

--- src/Main/component/PreviewProject.tsx
+++ src/Main/component/PreviewProject.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../style/main.module.css";
 import { PreviewProjectProps } from "../type/Main.type";
 
 const PreviewProject: React.FC<PreviewProjectProps> = (props) => {
-  console.log(props);
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.create_button}>
+    <div
+      onClick={() => navigate(`/project?projectSeq=${props.projectSeq}`)}
+      className={styles.create_button}
+    >
       <p>preview</p>
     </div>
   );
